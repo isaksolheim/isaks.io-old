20603aac5783d4729a7806d4496faeedf849b92e
wickdeeb <isak.m.h.s@gmail.com>
2020-09-29 20:37:10 +0200

    Fix navbar syntax

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,26 @@
 import * as React from 'react';
 import Link from 'gatsby-link';
 
-const Navbar = () => {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <Link className='text-link' to='/projects'>
-            projects
-          </Link>
-        </li>
-        <li>
-          <Link className='text-link' to='/tutorials'>
-            tutorials
-          </Link>
-        </li>
-        <li>
-          <Link className='text-link' to='/videos'>
-            videos
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  );
-};
+const Navbar = () => (
+  <nav>
+    <ul>
+      <li>
+        <Link className='text-link' to='/projects'>
+          projects
+        </Link>
+      </li>
+      <li>
+        <Link className='text-link' to='/tutorials'>
+          tutorials
+        </Link>
+      </li>
+      <li>
+        <Link className='text-link' to='/videos'>
+          videos
+        </Link>
+      </li>
+    </ul>
+  </nav>
+);
 
 export default Navbar;
