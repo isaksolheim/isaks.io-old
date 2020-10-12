1c24c59f296f599f1a27d41366b7c5e27398d056
wickdeeb <isak.m.h.s@gmail.com>
2020-10-12 17:18:27 +0200

    Add selected tab highlighting

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,42 @@
 import * as React from 'react';
 import Link from 'gatsby-link';
 
-const Navbar = () => (
-  <nav>
-    <ul>
-      <li>
-        <Link className='text-link' to='/projects'>
-          projects
-        </Link>
-      </li>
-      <li>
-        <Link className='text-link' to='/tutorials'>
-          tutorials
-        </Link>
-      </li>
-      <li>
-        <Link className='text-link' to='/videos'>
-          videos
-        </Link>
-      </li>
-    </ul>
-  </nav>
-);
+const Navbar = () => {
+  const url = typeof window !== 'undefined' ? window.location.href : '';
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link
+            className={`text-link ${
+              url.includes('projects') ? 'selected' : ''
+            }`}
+            to='/projects'
+          >
+            projects
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={`text-link ${
+              url.includes('tutorials') ? 'selected' : ''
+            }`}
+            to='/tutorials'
+          >
+            tutorials
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={`text-link ${url.includes('videos') ? 'selected' : ''}`}
+            to='/videos'
+          >
+            videos
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
