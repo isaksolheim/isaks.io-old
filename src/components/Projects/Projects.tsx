395cbc1b5e91c0290eb772eabe80301bef1af45a
wickdeeb <isak.m.h.s@gmail.com>
2021-07-30 14:16:14 +0200

    Add project component

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import Project from '../Project/Project';
 
 const Projects = () => (
   <React.Fragment>
     <h2>Projects</h2>
+    <Project
+      title='MyVinyl'
+      aboutText='iOS and Android app used to track vinyl plays.'
+      imgName='myvinyl.png'
+      techUsed={[
+        { name: 'TypeScript', techImgName: 'typescript.png' },
+        { name: 'React Native', techImgName: 'react.png' },
+        { name: 'Redux', techImgName: 'redux.png' },
+        { name: 'Sass', techImgName: 'sass.png' },
+        { name: 'Firebase', techImgName: 'firebase.png' },
+        { name: 'Discogs API', techImgName: 'discogs.png' },
+      ]}
+    />
   </React.Fragment>
 );
 
