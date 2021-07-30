b9d46d09da34e635d3693f8b3b2181b9c0a3f4d1
wickdeeb <isak.m.h.s@gmail.com>
2021-07-30 14:56:21 +0200

    Add more projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -12,9 +12,31 @@ const Projects = () => (
         { name: 'TypeScript', techImgName: 'typescript.png' },
         { name: 'React Native', techImgName: 'react.png' },
         { name: 'Redux', techImgName: 'redux.png' },
-        { name: 'Sass', techImgName: 'sass.png' },
         { name: 'Firebase', techImgName: 'firebase.png' },
         { name: 'Discogs API', techImgName: 'discogs.png' },
+        { name: 'Adobe XD', techImgName: 'adobexd.png' },
+      ]}
+    />
+    <Project
+      title='Beer In Review'
+      aboutText='iOS and Android app used to track and visualize beer consumtion.'
+      imgName='beerinreview.png'
+      techUsed={[
+        { name: 'TypeScript', techImgName: 'typescript.png' },
+        { name: 'React Native', techImgName: 'react.png' },
+        { name: 'Lottie', techImgName: 'lottie.png' },
+      ]}
+    />
+    <Project
+      title='Cloud'
+      aboutText='Physical cloud that waters plants and drops beer.'
+      imgName='cloud.png'
+      techUsed={[
+        { name: 'TypeScript', techImgName: 'typescript.png' },
+        { name: 'Node', techImgName: 'nodejs.png' },
+        { name: 'Python', techImgName: 'python.png' },
+        { name: 'Raspberry PI', techImgName: 'raspberrypi.png' },
+        { name: 'MQTT', techImgName: 'mqtt.png' },
       ]}
     />
   </React.Fragment>
