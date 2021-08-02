40fcba0d6de35953d7f899e9881b42761175544b
Isak Solheim <42111513+wickdeeb@users.noreply.github.com>
2021-08-02 17:03:46 +0200

    Add fa icons to navbar (#16)

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import TextLink from '../TextLink/text-link';
 import './navbar.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faLinkedin,
+  faMedium,
+  faGithub,
+  faYoutube,
+} from '@fortawesome/free-brands-svg-icons';
 
 const Navbar = () => {
   const links = [
-    { text: 'LinkedIn', url: 'https://www.linkedin.com/in/isak-solheim/' },
-    { text: 'Medium', url: 'https://medium.com/@isaksolheim' },
-    { text: 'GitHub', url: 'https://github.com/wickdeeb' },
-    { text: 'YouTube', url: 'https://www.youtube.com/c/isaks' },
+    {
+      text: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/isak-solheim/',
+      icon: faLinkedin,
+    },
+    { text: 'Medium', url: 'https://medium.com/@isaksolheim', icon: faMedium },
+    { text: 'GitHub', url: 'https://github.com/wickdeeb', icon: faGithub },
+    {
+      text: 'YouTube',
+      url: 'https://www.youtube.com/c/isaks',
+      icon: faYoutube,
+    },
   ];
 
   return (
@@ -15,7 +30,13 @@ const Navbar = () => {
       <ul>
         {links.map((link) => (
           <li key={link.text}>
-            <TextLink text={link.text} url={link.url} />
+            <a href={link.url}>
+              <FontAwesomeIcon
+                icon={link.icon}
+                style={{ marginRight: '4px', color: 'black' }}
+              />
+              {link.text}
+            </a>
           </li>
         ))}
       </ul>
