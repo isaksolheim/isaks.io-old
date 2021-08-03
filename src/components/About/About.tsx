0c52749755588d44c4fe318ec80a4b5fc5dd95e3
wickdeeb <isak.m.h.s@gmail.com>
2021-08-03 14:10:27 +0200

    Update about me section

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
-import TextLink from '../TextLink/text-link';
+import './about.scss';
 
 const About = () => (
   <React.Fragment>
     <h2>About 👋</h2>
-    <p>Hi, I'm Isak, a front-end developer currently living in Trondheim.</p>
     <p>
-      On this website you can find some of my{' '}
-      <TextLink url='/projects' text='projects' />,{' '}
-      <TextLink url='/tutorials' text='tutorials' />, and{' '}
-      <TextLink url='/videos' text='my videos' />. Also, check out my{' '}
-      <TextLink url='https://github.com/wickdeeb' text='github' /> and my{' '}
-      <TextLink url='https://www.youtube.com/c/isaks' text='youtube channel' />.
+      Hi, I'm Isak, a front-end developer currently living in Trondheim. I love
+      problem solving and building things for the web!
     </p>
+
+    <p>Here are a few technologies I've been working with recently:</p>
+    <ul className='tech-list'>
+      <li>
+        <a href='https://www.typescriptlang.org/'>TypeScript</a>
+      </li>
+      <li>
+        <a href='https://reactjs.org/'>React</a>
+      </li>
+      <li>
+        <a href='https://redux.js.org/'>Redux</a>
+      </li>
+      <li>
+        <a href='https://testing-library.com/'>React Testing Library</a>
+      </li>
+      <li>
+        <a href='https://storybook.js.org/'>Storybook</a>
+      </li>
+      <li>
+        <a href='https://firebase.google.com/'>Firebase</a>
+      </li>
+    </ul>
   </React.Fragment>
 );
 
