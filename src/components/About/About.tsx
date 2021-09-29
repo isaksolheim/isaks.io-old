8ae5b13c0881449598725901ae73736546c3eae3
Isak Solheim <42111513+isaksolheim@users.noreply.github.com>
2021-09-29 10:55:12 +0200

    Update About section (#21)

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,9 +5,29 @@ const About = () => (
   <React.Fragment>
     <h2>About 👋</h2>
     <p>
-      Hi, I'm Isak, a front-end developer currently living in Trondheim. I love
+      Hi, I'm Isak, a software developer currently living in Trondheim. I love
       problem solving and building things for the web!
     </p>
+
+    <p>
+      <b>Current projects:</b>
+    </p>
+    <ul>
+      <li>
+        🛰️ I'm learning C and building a satellite @{' '}
+        <a href='https://orbitntnu.com'>Orbit</a>
+      </li>
+      <li>
+        💻 I'm building a tool for students with Svelte and Tailwind @{' '}
+        <a href='https://github.com/dotkom'>Dotkom</a>
+      </li>
+      <li>
+        🔑 I'm developing and maintaing a CMS for the app Oslonøkkelen @{' '}
+        <a href='https://www.oslo.kommune.no/etater-foretak-og-ombud/oslo-origo/#gref'>
+          Oslo Origo
+        </a>
+      </li>
+    </ul>
 
     <p>Here are a few technologies I've been working with recently:</p>
     <ul className='tech-list'>
@@ -24,6 +44,9 @@ const About = () => (
         <a href='https://storybook.js.org/'>Storybook</a>
       </li>
       <li>
+        <a href='https://svelte.dev/'>Svelte</a>
+      </li>
+      <li>
         <a href='https://testing-library.com/'>React Testing Library</a>
       </li>
       <li>
@@ -34,6 +57,9 @@ const About = () => (
       </li>
       <li>
         <a href='https://kotlinlang.org/'>Kotlin</a>
+      </li>
+      <li>
+        <a href='https://tailwindcss.com/'>Tailwind</a>
       </li>
     </ul>
   </React.Fragment>
