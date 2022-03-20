6573aef4e68229dcab281bd3f7739019d97a76ac
isaksolheim <isak.m.h.s@gmail.com>
2022-03-20 20:57:00 +0100

    Update project link style

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -8,10 +8,7 @@ interface ProjectProps {
   title: string;
   aboutText: string;
   img: Object;
-  techUsed: {
-    name: string;
-    techImgName: string;
-  }[];
+  techUsed: string;
   projectLink?: string;
 }
 
@@ -23,31 +20,27 @@ const Project = ({
   projectLink,
 }: ProjectProps) => {
   return (
-    <li className='project-container'>
-      {projectLink ? (
-        <Link to={projectLink}>View project</Link>
-      ) : (
-        <p className='top-right-text'>In development</p>
-      )}
-      <GatsbyImage
-        image={img.childImageSharp.gatsbyImageData}
-        alt='project'
-        className='project-image'
-      />
-      <h3>{title}</h3>
-      <p>{aboutText}</p>
-      <h4>Technologies used</h4>
-      <ul>
-        {techUsed.map((tech) => (
-          <li key={tech.name}>
-            <figure className='image-container'>
-              <ImageMap imgName={tech.techImgName} />
-            </figure>
-            <label>{tech.name}</label>
-          </li>
-        ))}
-      </ul>
-    </li>
+    <Link to={projectLink} className='project-container'>
+      <li>
+        <p className='top-right-text'>
+          {projectLink ? 'Live' : 'In development'}
+        </p>
+        <div className='section-container'>
+          <div className='text-content'>
+            <h3>{title}</h3>
+            <p>{aboutText}</p>
+            <p>
+              <b>Tech:</b> {techUsed}
+            </p>
+          </div>
+          <GatsbyImage
+            image={img.childImageSharp.gatsbyImageData}
+            alt='project'
+            className='project-image'
+          />
+        </div>
+      </li>
+    </Link>
   );
 };
 
